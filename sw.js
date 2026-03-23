c70253866b7d826d0c747e9aad4621c22b09e347
gardiboy12-dotcom <gardiboy12@gmail.com>
2026-03-23 09:58:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'jaedanwang-v1';
+var CACHE = 'jaedanwang-v3';
 var FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
